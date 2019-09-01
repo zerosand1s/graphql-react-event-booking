aeadb444024384f4b0c28ff5c8cfd63300003b8a
Harshal <gangurdeharsh08@gmail.com>
2019-09-01 14:24:19 +0530

    create user model, modify the way we save events

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const expressGraphql = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -29,6 +31,17 @@ app.use('/graphql',
         date: String!
       }
 
+      type User {
+        _id: ID!
+        email: String!
+        password: String
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
+      }
+
       input EventInput {
         title: String!
         description: String!
@@ -42,6 +55,7 @@ app.use('/graphql',
 
       type RootMutation {
         createEvent(eventInput: EventInput): Event
+        createUser(userInput: UserInput): User
       }
 
       schema {
@@ -60,24 +74,54 @@ app.use('/graphql',
             throw err;
           });
       },
-      createEvent: (args) => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date)
-        });
+      createEvent: async (args) => {
+        try {
 
-        return event
-          .save()
-          .then((result) => {
-            console.log('Event saved: ', result);
-            return result._doc;
-          })
-          .catch(err => {
-            console.log('Error while creating event: ', err);
-            throw err;
+          const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: '5d6b81bfb7f5453b06154811'
           });
+
+          const eventCreated = await event.save();
+          const user = await User.findById('5d6b81bfb7f5453b06154811');
+
+          user.createdEvents.push(event);
+          await user.save();
+          console.log('Event saved: ', eventCreated);
+          return eventCreated._doc;
+
+        } catch (err) {
+          console.log('Error while creating event: ', err);
+          return err;
+        }
+      },
+      createUser: async (args) => {
+        try {
+
+          const existingUser = await User.findOne({ email: args.userInput.email });
+
+          if (existingUser) {
+            throw new Error('User exists already');
+          }
+
+          const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+
+          const userCreated = await user.save();
+          console.log('User saved: ', user);
+          return { ...userCreated._doc, password: null };
+
+        } catch (err) {
+          console.log('Error while creating user: ', err);
+          return err;
+        }
       }
     },
     graphiql: true
